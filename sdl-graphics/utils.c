7d128b161e9d94e51ad8532bdb2266c05f72e35d
Siddhartha Mishra <siddhartha.mish2000@gmail.com>
2022-04-11 07:50:15 +0100

    Fixed intersection testing code

--- sdl-graphics/utils.c
+++ sdl-graphics/utils.c
@@ -8,8 +8,17 @@ bool isIntersecting(Object *first, Object *second)
     SDL_Rect *obj1 = &first->rect;
     SDL_Rect *obj2 = &second->rect;
 
-    return 
-        MAX(obj1->x, obj2->x) - MIN(obj1->x, obj2->x) < obj1->w && 
-        MAX(obj1->y, obj2->y) - MIN(obj1->y, obj2->y) < obj2->h;
+    SDL_Rect *right     = obj1->x > obj2->x ? obj1 : obj2;
+    SDL_Rect *bottom    = obj1->y > obj2->y ? obj1 : obj2;
 
+    SDL_Rect *left      = right == obj1 ? obj2 : obj1;
+    SDL_Rect *top       = bottom == obj1 ? obj2 : obj1;
+
+    // This seems to produce code with less branches but I'm scared to leave it in
+    // SDL_Rect *left  = (SDL_Rect *)(-(long int)right + (long int)obj1 + (long int)obj2);
+    // SDL_Rect *top   = (SDL_Rect *)(-(long int)bottom + (long int)obj1 + (long int)obj2);
+
+    return 
+        (right->x < left->x + left->w) &&
+        (bottom->y < top->y + top->h);
 }
