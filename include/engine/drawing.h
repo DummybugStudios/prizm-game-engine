d36ddb74e3aff1277683cbd49af30f56b9929dce
Siddhartha Mishra <siddhartha.mish2000@gmail.com>
2022-04-18 00:27:42 +0100

    added lines, filled circles and API changes for drawing

--- include/engine/drawing.h
+++ include/engine/drawing.h
@@ -5,10 +5,18 @@
 #define ENGINE_DRAWING_H
 
 #include <engine/collider.h>
+#include <engine/constants.h>
 
-void draw_circle (Collider *collider);
-void draw_rectangle (Collider *collider);
-void draw_collider (Collider *collider); 
+static inline void putpixel(int x, int y, int color)
+{
+    vramaddress[x + WIDTH *y] = color;
+}
+
+void draw_circle (int x0, int y0, int radius, int color);
+void draw_circle_filled(int x0, int y0, int radius, int color);
+void draw_rectangle_filled(int x, int y, int width, int height, int color);
+void draw_line(int x1, int y1, int x2, int y2, int color);
+void draw_collider (Collider *collider);
 
 #endif // ENGINE_DRAWING_H
 
--- src/drawing.c
+++ src/drawing.c
@@ -1,34 +1,24 @@
+#ifdef __SH4A__
+#include <stdlib.h>
 #include <engine/drawing.h>
 #include <fxcg/display.h>
 #include <engine/constants.h> 
 
-// TODO: add color option
-void draw_rectangle(Collider *collider)
+void draw_rectangle_filled(int x, int y, int width, int height, int color)
 {
-    // start at the top left
-    float x = collider->x;
-    float y = collider->y;
-    float sizey = collider->collider.rect.height;
-    float sizex = collider->collider.rect.width;
-
-    int startpoint = (y-sizey)*LCD_WIDTH_PX + (x-sizex); 
-    for (int i = startpoint; i < startpoint+sizex; i++)
+    int startpoint = (y-height)*LCD_WIDTH_PX + (x-width); 
+    for (int i = startpoint; i < startpoint+width; i++)
     { 
-        for (int j = 0; j < sizey; j++)
-        { 
-            // make it green for now 
-            vramaddress[i+LCD_WIDTH_PX*j] = 0b0000011111100000;
+        for (int j = 0; j < height; j++)
+        {
+            putpixel(x,y,color); 
         }
     }
 }
 
 // draw circle outline
-void draw_circle (Collider *collider)
+void draw_circle (int x0, int y0, int radius,int color)
 {
-    int x0 = collider->x;
-    int y0 = collider->y;
-    int radius = collider->collider.circle.radius;
-
     int x = radius - 1;
     int y = 0;
 
@@ -37,17 +27,56 @@ void draw_circle (Collider *collider)
 
     int err = dx - (radius << 1);
 
-    int color = 0b0000011111100000;
     while (x >= y)
     {
-        vramaddress[x0 + x + (y0 + y) * WIDTH] = color;
-        vramaddress[x0 + y + (y0 + x) * WIDTH] = color;
-        vramaddress[x0 - y + (y0 + x) * WIDTH] = color;
-        vramaddress[x0 - x + (y0 + y) * WIDTH] = color;
-        vramaddress[x0 - x + (y0 - y) * WIDTH] = color;
-        vramaddress[x0 - y + (y0 - x) * WIDTH] = color;
-        vramaddress[x0 + y + (y0 - x) * WIDTH] = color;
-        vramaddress[x0 + x + (y0 - y) * WIDTH] = color;
+        putpixel(x0 + x, y0 + y, color);
+        putpixel(x0 + y, y0 + x, color);
+        putpixel(x0 - y, y0 + x, color);
+        putpixel(x0 - x, y0 + y, color);
+        putpixel(x0 - x, y0 - y, color);
+        putpixel(x0 - y, y0 - x, color);
+        putpixel(x0 + y, y0 - x, color);
+        putpixel(x0 + x, y0 - y, color);
+
+
+        if (err <= 0)
+        {
+            y++;
+            err += dy;
+            dy += 2;
+        }
+        
+        if (err > 0)
+        {
+            x--;
+            dx += 2;
+            err += dx - (radius << 1);
+        }
+    }
+
+}
+
+void draw_circle_filled(int x0, int y0, int radius,int color)
+{
+    int x = radius - 1;
+    int y = 0;
+
+    int dx,dy;
+    dx = dy = 1;
+
+    int err = dx - (radius << 1);
+
+    while (x >= y)
+    {
+        draw_line(x0,y0,x0 + x, y0 + y,color);
+        draw_line(x0,y0,x0 + y, y0 + x,color);
+        draw_line(x0,y0,x0 - y, y0 + x,color);
+        draw_line(x0,y0,x0 - x, y0 + y,color);
+        draw_line(x0,y0,x0 - x, y0 - y,color);
+        draw_line(x0,y0,x0 - y, y0 - x,color);
+        draw_line(x0,y0,x0 + y, y0 - x,color);
+        draw_line(x0,y0,x0 + x, y0 - y,color);
+
 
         if (err <= 0)
         {
@@ -71,11 +100,57 @@ void draw_collider(Collider *collider)
     switch (collider->type)
     {
         case BOX_COLLIDER:
-        draw_rectangle(collider);
+        draw_rectangle_filled(
+            collider->x,
+            collider->y,
+            collider->collider.rect.width,
+            collider->collider.rect.height,
+            0b0000011111100000 // green for now
+            );
+
         break;
 
         case CIRCLE_COLLIDER:
-        draw_circle(collider);
+        draw_circle(
+            collider->x,
+            collider->y,
+            collider->collider.circle.radius,
+            0x3f << 5
+        );
         break;
     }
 }
+
+void draw_line(int x0, int y0, int x1, int y1, int color)
+{
+    int dx = abs(x1 - x0);
+    int sx = x0 < x1 ? 1 : -1;
+    int dy = -abs(y1 - y0);
+    int sy = y0 < y1 ? 1 : -1;
+
+    int err = dx + dy;
+
+    while (1)
+    {
+        putpixel(x0,y0,color);
+        if (x0 == x1 && y0 == y1) break; 
+
+        int e2 = err << 1;
+
+        if (e2 >= dy)
+        {
+            if (x0 == x1) break;
+            err += dy;
+            x0 += sx;
+        }
+        if (e2 <= dx)
+        {
+            if (y0 == y1) break;
+            err += dx;
+            y0 += sy; 
+        }
+    }
+
+}
+
+#endif // __SH4A__
